efff3e0dc3cd632c43d9dd28a9b62065df07aeb9
Xiaoyu <xiaoyu.hu@berkeley.edu>
2023-07-13 23:05:36 -0600

    change base to allow relative img paths in production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/etsy-tailwind-project-v2",
+  base: "./",
   plugins: [react()],
 })
